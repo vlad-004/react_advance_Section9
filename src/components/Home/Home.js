a296830b954c68fc7a71359853150588b2226ced
vlad-004 <prostoy-vlad@rambler.ru>
2023-03-07 16:40:49 +0200

    автовыравнивание после vs code. По правде старая версия мне больше нравилась. Надо будет перенастроить автовыравнивание чтобы не делала такие большие отступы ну или переходить на веб шторм попробовать.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,11 @@ import Card from "../UI/Card/Card";
 import styles from "./Home.module.css";
 
 const Home = (props) => {
-  return (
-    <Card className={styles.home}>
-      <h1>Рады Вас Видеть Снова!</h1>
-    </Card>
-  );
+    return (
+        <Card className={styles.home}>
+            <h1>Рады Вас Видеть Снова!</h1>
+        </Card>
+    );
 };
 
 export default Home;
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -4,15 +4,15 @@ import Navigation from "./Navigation";
 import styles from "./MainHeader.module.css";
 
 const MainHeader = (props) => {
-  return (
-    <header className={styles["main-header"]}>
-      <h1>React Advanced</h1>
-      <Navigation
-        isLoggedIn={props.isAuthenticated}
-        onLogout={props.onLogout}
-      />
-    </header>
-  );
+    return (
+        <header className={styles["main-header"]}>
+            <h1>React Advanced</h1>
+            <Navigation
+                isLoggedIn={props.isAuthenticated}
+                onLogout={props.onLogout}
+            />
+        </header>
+    );
 };
 
 export default MainHeader;
