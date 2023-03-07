162c147c5c93eb947f980be7dc4f4d3ca4d6e075
vlad-004 <prostoy-vlad@rambler.ru>
2023-03-07 16:36:25 +0200

    пример использования useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,11 +7,23 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    //когда будет перезапуск этого компонента App.js то анонимная функция из первого аргумента useEffect не будет перезапускаться,
+    // исключение первый запуск скрипта, так как массив dependens считается измененным.
+    //Если deps пустой массив, значит изменятся нечему будет, и ананонимка будет запускаться только 1 раз.
+    useEffect(() => {
+        const storedLoginInfo = localStorage.getItem('isLoggedIn');
+        if (storedLoginInfo === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
   const loginHandler = (email, password) => {
+      localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+      localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
