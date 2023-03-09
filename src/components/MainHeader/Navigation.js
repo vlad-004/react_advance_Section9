8f840167b43510a888b729f05c36098d0f213b3f
vlad-004 <prostoy-vlad@rambler.ru>
2023-03-09 16:32:14 +0200

    использование context api

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,28 +1,36 @@
 import React from "react";
 
 import styles from "./Navigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
     return (
-        <nav className={styles.nav}>
-            <ul>
-                {props.isLoggedIn && (
-                    <li>
-                        <a href="/">Пользователи</a>
-                    </li>
-                )}
-                {props.isLoggedIn && (
-                    <li>
-                        <a href="/">Админ</a>
-                    </li>
-                )}
-                {props.isLoggedIn && (
-                    <li>
-                        <button onClick={props.onLogout}>Выйти</button>
-                    </li>
-                )}
-            </ul>
-        </nav>
+        <AuthContext.Consumer>
+            {(context) => {
+                return (
+                    <nav className={styles.nav}>
+                        <ul>
+                            {context.isLoggedIn && (
+                                <li>
+                                    <a href="/">Пользователи</a>
+                                </li>
+                            )}
+                            {context.isLoggedIn && (
+                                <li>
+                                    <a href="/">Админ</a>
+                                </li>
+                            )}
+                            {context.isLoggedIn && (
+                                <li>
+                                    <button onClick={props.onLogout}>Выйти</button>
+                                </li>
+                            )}
+                        </ul>
+                    </nav>
+                );
+            }}
+
+        </AuthContext.Consumer>
     );
 };
 
